ee2255735100cfb2c9e05689af7ff0440feddaff
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-12 15:21:57 +0600

    client moved

--- client/src/main/java/tech/lapsa/insurance/dao/EJBConstants.java
+++
@@ -1,7 +0,0 @@
-package tech.lapsa.insurance.dao;
-
-public interface EJBConstants {
-
-    public static final String APPLICATION_NAME = "insurance-dao-app";
-    public static final String MODULE_NAME = "insurance-dao";
-}
